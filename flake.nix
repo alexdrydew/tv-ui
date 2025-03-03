b9afd88751f82ea357d6ad829b3a4e15ed1d8156
alexdrydew <aleksey.suhorosov@gmail.com>
2025-03-03 15:27:26 +0100

    neutralino -> tauri

--- flake.nix
+++ flake.nix
@@ -1,23 +1,51 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import inputs.nixpkgs {inherit system;};
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
+        };
       in {
         shellHook = ''
           $SHELL
         '';
         devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs_23
-            pkgs.nodePackages.pnpm
-            pkgs.nodePackages.typescript
-            pkgs.nodePackages.typescript-language-server
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+          ];
+
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+            (pkgs.rust-bin.stable.latest.default.override {extensions = ["rust-src"];})
+
+            nodePackages.pnpm
+            nodePackages.typescript
+            nodePackages.typescript-language-server
           ];
         };
       }
