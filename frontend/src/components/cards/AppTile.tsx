07a68d3dfc3bc69c2905c44011fb5dc61b21cdf2
alexdrydew <alexdrydew@toloka.ai>
2025-04-05 20:32:30 +0200

    fixes

--- frontend/src/components/cards/AppTile.tsx
+++ frontend/src/components/cards/AppTile.tsx
@@ -48,7 +48,7 @@ export function AppTile({
       <ContextMenuTrigger>
         <Button
           ref={buttonRef}
-          className={cn("flex flex-col items-center w-64 h-64")}
+          className={cn("flex flex-col items-center w-64 h-64 relative")}
           onClick={onSelect}
           onMouseOver={focusSelf}
           onFocus={focusSelf}
@@ -56,14 +56,18 @@ export function AppTile({
           <div className="flex-1 flex items-center justify-center">
             <img src={icon} alt={name} className="w-32 h-32" />
             {isRunning && (
-              <div className="absolute bottom-2 right-2 w-3 h-3 bg-green-500 rounded-full ring-2 ring-background" />
+              <div className="absolute bottom-2 right-2 w-3 h-3 bg-green-500 rounded-full" />
             )}
           </div>
           <h2 className="text-xl md:text-2xl font-bold mt-4">{name}</h2>
         </Button>
       </ContextMenuTrigger>
       <ContextMenuContent className="w-64">
-        <ContextMenuItem onClick={onKill} variant="destructive">
+        <ContextMenuItem
+          disabled={!isRunning}
+          onClick={onKill}
+          variant="destructive"
+        >
           Kill
         </ContextMenuItem>
       </ContextMenuContent>
