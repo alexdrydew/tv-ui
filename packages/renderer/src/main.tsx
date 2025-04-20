29147119764e67f06831716ac4b10f74560748b9
alexdrydew <alexdrydew@toloka.ai>
2025-04-20 17:02:08 +0200

    move delay to js

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,11 +1,19 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode, Suspense, lazy } from 'react';
 import './index.css';
-import { StrictMode } from 'react';
-import './index.css';
+
+const delay = import.meta.env.MODE === 'development' ? 500 : 0;
+const App = lazy(() => {
+    return new Promise((resolve) => {
+        // @ts-expect-error App type is lazy
+        setTimeout(() => resolve(import('./App.tsx')), delay);
+    });
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <Suspense fallback={<div>Waiting for preload to initialize...</div>}>
+            <App />
+        </Suspense>
     </StrictMode>,
 );
